b8ed2961e4eae2274e30f111f071571a3cee8c5b
William GAINNIER <william.gainnier@edu.devinci.fr>
2023-03-04 12:34:37 +0100

    Server finished

--- server/sandbox.js
+++ server/sandbox.js
@@ -1,21 +1,51 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
+const montlimart = require('./eshops/montlimart')
+const circlesportswear = require('./eshops/circlesportswear')
+let products = [];
+let a = [];
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
+//recupere tout les items d'un lien et l'affiche
+//A FAIRE : les 2 scrappers circle et Montlimart + stocker et sauvegarder dans un JSON File tout les resultats
+//, 'https://www.montlimart.com/101-t-shirts', 'https://www.circlesportswear.com/pages/vetement-running'] 'https://www.dedicatedbrand.com/en/men/news'
+
+async function sandbox (eshop = 'https://www.montlimart.com/101-t-shirts') {
   try {
     console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
 
-    const products = await dedicatedbrand.scrape(eshop);
+    products = await dedicatedbrand.scrape('https://www.dedicatedbrand.com/en/men/news');
+    a = await montlimart.scrape('https://www.montlimart.com/101-t-shirts');
+    products = products.concat(a)
+    a = await circlesportswear.scrape('https://shop.circlesportswear.com/collections/t-shirts-homme') 
+    products = products.concat(a)
 
     console.log(products);
     console.log('done');
+    Savefunc(products);
     process.exit(0);
   } catch (e) {
     console.error(e);
     process.exit(1);
   }
+
+  
 }
 
+//rajouter un stockage dans un Json File des 3 marques. 
 const [,, eshop] = process.argv;
-
 sandbox(eshop);
+
+function Savefunc(products)
+{
+const chemin = 'products.json';
+const fs = require('fs');
+const jsonData = JSON.stringify(products, null, 2);
+fs.writeFileSync('products.json', jsonData, (err) => {
+  if (err) throw err;
+  console.log('Data written to file');
+});
+}
+
+
+
+
