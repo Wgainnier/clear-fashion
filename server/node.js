375c7279627e207c40ff6f1e38cdd93f8f8a650c
William GAINNIER <william.gainnier@edu.devinci.fr>
2023-03-06 21:06:03 +0100

    Connection + insertion des produits dans la DB

--- server/node.js
+++ server/node.js
@@ -1,20 +1,40 @@
+const fs = require('fs');
+let products = [];
+function getProducts() {
+    try {
+      const data = fs.readFileSync('products.json', 'utf8');
+      const products = JSON.parse(data).map(function(product) {
+        return {
+          name: product.name,
+          price: product.price,
+          marque: product.marque
+        };
+      });
+      return products;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+  }
+products = getProducts()
+
 const {MongoClient} = require('mongodb');
 const MONGODB_URI = 'mongodb+srv://clearfashion:abcde@clearfashion.776670c.mongodb.net/?retryWrites=true&w=majority';
 const MONGODB_DB_NAME = 'clearfashion';
-let products = [];
+const client = new MongoClient(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function listDatabases(){
-    products = sandbox()
-    
-
- 
-    const client = await MongoClient.connect(MONGODB_URI, {'useNewUrlParser': true});
+async function insert(){
+    await client.connect();
+    try{
     const db =  client.db(MONGODB_DB_NAME)
     const collection = db.collection('products');
-    const result = collection.insertMany(products);
+    const result = await collection.insertMany(products);
     console.log(result);
-    await client.close();
+    }
+    finally{
+    client.close();
     console.log("finish")
+    }
 };
 
-listDatabases()
+insert()
